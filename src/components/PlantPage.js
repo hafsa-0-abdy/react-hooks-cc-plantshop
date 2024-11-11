85f0a69a7006b3a8b7dd7dca1ffdaa5cddc7c7bc
Habsa Abdirizack <habsa.abdirizack@student.moringaschool.com>
2024-11-11 10:05:40 +0300

    deploy render

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -8,7 +8,7 @@ function PlantPage() {
   const [searchTerm, setSearchTerm] = useState(""); 
 
   useEffect(() => {
-    fetch("http://localhost:6001/plants")
+    fetch("https://react-hooks-cc-plantshop-pmlp.onrender.com/plants")
       .then((res) => res.json())
       .then((data) => setPlants(data));
   }, []);
